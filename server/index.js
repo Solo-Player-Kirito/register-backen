a0f7bccf37ef641b48d87157fd1d8570dfabf2a7
Solo-Player-Kirito <sonusonu68259@gmail.com>
2024-08-02 19:52:53 +0530

    folder change

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
-app.use("/", require("./routes/route"));
+app.use("/", require("../routes/route"));
 app.listen(port, () => {
   console.log("Server running on port : " + port);
 });
