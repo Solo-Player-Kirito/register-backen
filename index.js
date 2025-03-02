cac4c86a0c973bf81a354e9a46de758b419b0c8a
Solo-Player-Kirito <sonusonu60019@gmail.com>
2025-03-02 16:56:13 +0530

    webhook add

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ require("dotenv").config();
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
+
+const schema = new mongoose.Schema({
+  data: Object,
+});
+const wmodel = mongoose.model("webhoks", schema);
 
 // Middleware
 app.use(cors());
@@ -30,7 +35,7 @@ const course = require("./routes/trainig/course_route");
 const enroll = require("./routes/trainig/user_route");
 const certificate = require("./routes/trainig/certificate_routes");
 const banner = require("./funtions/banners");
-const blogs = require("./routes/blog_routes/blog_route")
+const blogs = require("./routes/blog_routes/blog_route");
 const jobs = require("./routes/job_routes/job_routes");
 app.use("/", form);
 app.use("/", imgroutes);
@@ -41,13 +46,35 @@ app.use("/", enroll);
 app.use("/", certificate);
 app.use("/", banner);
 app.use("/blogs", blogs);
-app.use("/jobs",jobs);
+app.use("/jobs", jobs);
 
 app.use("/img", express.static(path.join(__dirname, "img")));
 app.get("/", (req, res) => {
-  res.send("jobs profile 23-01-2025 ");
+  res.send(" webhook test  : 02 - 03 - 2025 ");
 });
+app.post("/webhook", async (req, res) => {
+  try {
+    console.log("webhook ok listening : ");
+    console.log(req.body);
 
+    const data = await wmodel.create({
+      data: req.body,
+    });
+    res.status(200).send("ok web");
+  } catch (err) {
+    console.log("some error in the webhook : ", err);
+    res.status(500).send("internal server error in the webhook : )");
+  }
+});
+app.get("/web", async (req, res) => {
+  try {
+    const data = await wmodel.find();
+    res.status(200).send(data);
+  } catch (err) {
+    console.log("some error : ", err);
+    res.status(500).send("inter web error on fetch");
+  }
+});
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
